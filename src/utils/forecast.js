94a62c96b8340933784c651cebf393ac44bac8cc
KumarNaveenKumar <naveenchaudahryyyy@gmail.com>
2022-01-27 22:13:06 +0530

    changing units of temperature from fahrenheit to celcius.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
 
-    const url = 'http://api.weatherstack.com/current?access_key=e9976a378b2a994e40e69978a67668ef&query=' + latitude + ',' + longitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=e9976a378b2a994e40e69978a67668ef&query=' + latitude + ',' + longitude + '&units=m'
     
     request( {url, json:true}, (error, { body } ) => {
         if(error) {
